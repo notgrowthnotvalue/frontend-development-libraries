368dde650e65c8b42725ed17c75367dbc154751d
notgrowthnotvalue <notgrowthnotvalue@gmail.com>
2023-01-31 23:29:40 -0500

    feat: added examples

---
+++ react-snippet2.js
@@ -0,0 +1,22 @@
+class CampSite extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <Camper />
+            </div>
+        );
+    }
+};
+// Change code below this line
+
+const Camper = (props) => {
+    { /* Change code below this line */ }
+    return <p>{props.name}</p>
+    { /* Change code above this line */ }
+};
+
+Camper.defaultProps = { name: 'CamperBot' }
+Camper.propTypes = { name: PropTypes.string.isRequired }
