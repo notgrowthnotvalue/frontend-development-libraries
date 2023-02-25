4426300a6443fa045a5fd7e4aa7c9448fa5c0eaa
notgrowthnotvalue <notgrowthnotvalue@gmail.com>
2023-02-24 23:57:10 -0500

    new example

---
+++ fetch-POST-async-example/main.js
@@ -0,0 +1,49 @@
+import { renderJsonResponse, renderRawResponse, renderResponse } from './helperFunctions.js'
+import { REBRANDLY_API_KEY } from './api-key.js' // saved in separate file
+
+
+// Information to reach API
+const apiKey = REBRANDLY_API_KEY;
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous functions
+const shortenUrl = async () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten });
+    try {
+        const response = await fetch(
+            url,
+            {
+                method: 'POST',
+                body: data,
+                headers: {
+                    'Content-type': 'application/json',
+                    'apikey': apiKey
+                }
+            }
+
+        );
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            renderResponse(jsonResponse);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while (responseField.firstChild) {
+        responseField.removeChild(responseField.firstChild);
+    }
+    shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
